2ba9029d2c81b0c47332f618cc76c58baa280beb
nick_nak <nikita.nackaznoy@gmail.com>
2020-10-29 16:00:34 +0300

    add data

--- test/test1.cpp
+++ test/test1.cpp
@@ -26,10 +26,6 @@ TEST(InitListTest1, InvalidPositionName) {
   ASSERT_EQ(init_node(NULL), nullptr);
 }
 
-TEST(MainTest, ReadFromFile) {
-  ASSERT_EQ(init_node(NULL), nullptr);
-}
-
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
